73f876bffe1ebfd8684471c86c5e15293bc9664a
MuhammadRifal22 <rifalmuhammad797@gmail.com>
2020-12-20 10:24:32 +0700

    Add project files.

---
+++ ServerConfigRest_20180140121_Muhammad Rifal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20180140121_Muhammad_Rifal;
+
+namespace ServerConfigRest_20180140121_Muhammad_Rifal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
